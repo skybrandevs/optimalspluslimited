ea079f20bebd70f6799a121ba0f80fb929b536b3
johnson chidubem <johnsonchidubem@johnsons-MacBook-Pro.local>
2019-12-30 18:30:38 +0100

    added new link

--- src/components/pages/privacy.js
+++ src/components/pages/privacy.js
@@ -5,7 +5,7 @@ import guide from "../../images/guide.png";
 
 
 
-const Privacys = () => (
+const Privacy = () => (
 
 
     <div>
@@ -431,5 +431,5 @@ about a child under the age of 13, please contact us.</p>
 
     );
 
-    export default Privacys;
+    export default Privacy;
     
